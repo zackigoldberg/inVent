66f85a7161e8b4019ae89d9ce94a0c18a71ee183
zackigoldberg <zackigoldberg@gmail.com>
2018-11-27 11:45:04 -0500

    Add project files.

---
+++ inVent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(inVent.Startup))]
+namespace inVent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
